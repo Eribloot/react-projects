41b7faab6a8ef853dd474b77061acc9303b8fd8d
eribloot <Fenton_Eric@pm.me>
2022-06-18 09:24:48 -0500

    removal and refresh

--- 2-tours/src/App.js
+++ 2-tours/src/App.js
@@ -6,8 +6,14 @@ const url = 'https://course-api.com/react-tours-project';
 
 export default function App()
 {
-  const [loading, setLoading ] = useState(true);
-  const [tours, setTours ] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+
+    setTours(newTours);
+  }
 
   const fetchTours = async () => {
     setLoading(true);
@@ -38,9 +44,21 @@ export default function App()
     );
   }
 
-return(
-  <main>
-    <Tours tours={tours}/> 
-  </main>
-)
+  if(tours.length === 0) {
+    return <main>
+      <div className='title'>
+        <h2>No more tours left!</h2>
+        <button className='btn' onClick={() => fetchTours()}>
+          refresh...
+        </button>
+      </div>
+    </main>
+  }
+
+
+  return(
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/> 
+    </main>
+  );
 }
