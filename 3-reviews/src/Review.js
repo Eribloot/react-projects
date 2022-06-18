05b52e5ddfebe79e41fad3a080853b0c9f3c5c69
eribloot <Fenton_Eric@pm.me>
2022-06-18 09:55:12 -0500

    setup reviews project

---
+++ 3-reviews/src/Review.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import people from './data';
+import { FaChevronLeft, FaCheveronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  return <h2>review component!</h2>;
+}
+
+export default Review;
