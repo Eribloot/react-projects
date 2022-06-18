49017431a0ef1fe585084ff01729b0756088b4b2
eribloot <Fenton_Eric@pm.me>
2022-06-18 11:08:41 -0500

    Accordion setup

---
+++ 4-accordion/src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from "./Question";
+
+function App() {
+  return <h2>Accordion project setup</h2>;
+}
+
+export default App;
